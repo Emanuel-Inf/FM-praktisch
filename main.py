dc59e38234b50e3b1900df276a6b9496843db9f7
MaxWeise <maxfencing@web.de>
2024-06-10 22:42:59 +0200

    chore: add main guard

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ def main():
     excel_data = data_reader.read_xlsx("daten/xlsx/00000001.xlsx")
 
     list_of_decks = data_reader._parse_data_to_list_with_deck_objects(excel_data)
-    
-    #Beispiel um das letzte Deck innerhalb des Datensatzes zu bekommen
-    print(list_of_decks[323])
+
+    # Beispiel um das letzte Deck innerhalb des Datensatzes zu bekommen
+    print(list_of_decks)
+    print(f"{len(list_of_decks) = }")
 
 
-main()
+if __name__ == "__main__":
+    main()
+
