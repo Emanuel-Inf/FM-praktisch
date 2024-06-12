b6d3fe9d9fe51f8187c0fa57fe1244b8014f0073
MaxWeise <maxfencing@web.de>
2024-06-12 16:47:29 +0200

    feat: add method to count a given card per deck

--- deck.py
+++ deck.py
@@ -1,21 +1,16 @@
+import dataclasses
+
+
+@dataclasses.dataclass
 class Deck:
     """Definiton of a Pokemon. This class is immutabel and sortable."""
+
     deck_num: str
     name: str
     main_deck: list[str]
 
-    def __init__(self, name: str, deck_num: str, main_deck: list[str]):
-        self.name = name
-        self.deck_num = deck_num
-        self.main_deck = main_deck
-    
-    def __repr__(self):
-        return (f"Deck(name={self.name!r}, deck_num={self.deck_num!r}, "
-                f"main_deck={self.main_deck!r})")
-    
-    def __eq__(self, other):
-        if isinstance(other, Deck):
-            return (self.name == other.name and
-                    self.deck_num == other.deck_num and
-                    self.main_deck == other.main_deck)
-        return False
+    def contains_card(self, card_id: str) -> int:
+        """Search for a card in the deck."""
+        counted_cards = self.main_deck.count(card_id)
+        return counted_cards
+
