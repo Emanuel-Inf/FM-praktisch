7eaa4e7fa88a0d3ef609b1eb73d67d4a9bacc173
MaxWeise <maxfencing@web.de>
2024-06-15 15:55:24 +0200

    feat: implement random sampling of decks

--- main.py
+++ main.py
@@ -6,40 +6,45 @@ Date: 30.11.2022
 
 import functools
 import os
+import random
 
 import data_reader
 
 CSV_DIRECTORY: str = "./daten/csv/"
-SEARCHED_CARD: str = "83764718"
-SEARCH_CARD_LIST = ["83764718", "47826112", "3643300", "53804307", "81843628", "36553319"]
+SEARCH_CARDS = ["83764718", "47826112", "3643300", "53804307", "81843628", "36553319"]
+SAMPLE_SIZE: int = 0
 
 
 def main():
     """Executes the main function."""
 
-    files = os.listdir(CSV_DIRECTORY)  # Get all files in directory
+    # Read all data from the data directory
+    files = os.listdir(CSV_DIRECTORY)
     decks = []
     for f in files:
         csv_data = data_reader.read_csv(f"{CSV_DIRECTORY}/{f}")
         list_of_decks = data_reader.parse_data_to_list_with_deck_objects(csv_data)
-        print(f"Lade Datei mit {len(list_of_decks)} Decks")
         decks.append(list_of_decks)
 
+    # Prepare the data
     flattened_decks = functools.reduce(lambda x, y: x + y, decks, [])
+    random_sample = random.sample(
+        flattened_decks, SAMPLE_SIZE if SAMPLE_SIZE > 0 else len(flattened_decks)
+    )
 
-    count = 0
-    for deck in flattened_decks:
-        count += deck.contains_card(SEARCHED_CARD)
-
-    #print(f"The card {SEARCHED_CARD} was counted {count} times")
-    dict_cards = {}
-    for card in SEARCH_CARD_LIST:
-        dict_cards[card] = 0
+    # Count the frequency of the specified cards in decks
+    card_frequency = {}
+    for card in SEARCH_CARDS:
+        card_frequency[card] = 0
         count = 0
-        for deck in flattened_decks:
+        for deck in random_sample:
             count += deck.contains_card(card)
-        dict_cards[card] +=  count
+        card_frequency[card] += count
 
-    print(dict_cards)
+    print(card_frequency)
+
+    # TODO: Calculate the confidence intervall
+
+
 if __name__ == "__main__":
     main()
